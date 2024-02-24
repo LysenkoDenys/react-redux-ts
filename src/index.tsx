5a43fd36dfedc490882f5ceb530b0cf675d05884
Lysenko <lysenkoden@gmail.com>
2024-02-24 09:27:01 +0100

    create store and action fetch from JSONplaceholder

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-class App extends React.Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.querySelector('#root'));
-
-export default App;
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
