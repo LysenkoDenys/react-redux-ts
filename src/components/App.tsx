eadffe2ca721af4c627c6cb0ec6231e4c7eafad2
Lysenko <lysenkoden@gmail.com>
2024-02-25 09:22:22 +0100

    add action delete todo, add functionality to delete todo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchTodos, Todo } from '../actions';
+import { deleteTodo, fetchTodos, Todo } from '../actions';
 import { StoreState } from '../reducers';
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
 }
 
-export class App extends Component<AppProps> {
+interface AppState {
+  fetching: boolean;
+}
+
+class _App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { fetching: false };
+  }
+
+  // componentDidMount(): void {
+  //   this.props.fetchTodos();
+  // }
+  componentDidUpdate(prevPropps: AppProps): void {
+    if (!prevPropps.todos.length && this.props.todos.length) {
+      this.setState({ fetching: false });
+    }
+  }
+
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+    this.setState({ fetching: true });
+  };
+
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return (
+        <div
+          onClick={() => this.onTodoClick(todo.id)}
+          key={todo.id}
+        >{`${todo.id}. ${todo.title}`}</div>
+      );
+    });
+  }
+
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        <br />
+        {this.state.fetching ? 'Loading...' : null}
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
@@ -18,4 +64,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
   return { todos };
 };
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
